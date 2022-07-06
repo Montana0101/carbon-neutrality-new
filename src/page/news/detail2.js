542d0aa4791ffb1f656963e3324c62b4e8a32a48
montana <montana0101@outlook.com>
2022-07-06 16:17:46 +0800

    存档

--- src/page/news/detail2.js
+++ src/page/news/detail2.js
@@ -130,7 +130,7 @@ function NewsDetail2(props) {
                     alignItems: "flex-start", justifyContent: "center",
                     borderLeft: CutLine, borderRight: CutLine,
                 }}>
-                    <div onClick={() => { history.push("/news") }}>
+                    <div onClick={() => { history.push("/news/1") }}>
                         <IconFont type="icon-tuichu" style={{ color: ThemeColor, margin: "0 0.1rem 0 0", fontSize: "0.12rem" }} />
                         <a href="#" style={{ fontSize: "0.12rem", textDecoration: "underline", color: ThemeColor, fontWeight: "400" }}>
                         沪科〔2021〕497号 关于同意成立上海碳中和技术创新联盟的批复</a>
