00b6a7ef162a8f3ad34501903960acc076691a59
montana <montana0101@outlook.com>
2022-07-13 15:50:04 +0800

    双碳详情6

--- src/page/consult/sub/sub6.js
+++ src/page/consult/sub/sub6.js
@@ -122,7 +122,7 @@ const ConsultSub6 = () => {
         }}
       >
         <div
-          className="col"
+          className="col_t"
           style={{
             color: ThemeColor,
             fontWeight: "bold",
@@ -196,7 +196,7 @@ const ConsultSub6 = () => {
             }}
           >
             <div
-              className="col"
+              className="col_t"
               style={{
                 color: ThemeColor,
                 fontWeight: "bold",
@@ -233,7 +233,7 @@ const ConsultSub6 = () => {
             }}
           >
             <div
-              className="col"
+              className="col_t"
               style={{
                 color: ThemeColor,
                 fontWeight: "bold",
@@ -299,7 +299,7 @@ const ConsultSub6 = () => {
             }}
           >
             <div
-              className="col"
+              className="col_t"
               style={{
                 color: ThemeColor,
                 fontWeight: "bold",
@@ -369,7 +369,7 @@ const ConsultSub6 = () => {
             }}
           >
             <div
-              className="col"
+              className="col_t"
               style={{
                 color: ThemeColor,
                 fontWeight: "bold",
@@ -383,8 +383,205 @@ const ConsultSub6 = () => {
           </div>
         </div>
 
-       
-      
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            marginTop: "0.5rem",
+            height: "2.3rem",
+          }}
+        >
+          <div
+            style={{
+              flex: 4,
+              display: "flex",
+              flexDirection: "column",
+              height: "100%",
+              //   alignItems: "center",
+              paddingRight: "0.3rem",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              className="col_t"
+              style={{
+                color: ThemeColor,
+                fontWeight: "bold",
+              }}
+            >
+              CO2地址封存
+            </div>
+            <p className="col">
+              是指通过工程技术手段将捕集的CO2封存于地质构造中，实现与大气长期隔绝但不产生附带经济效益的过程。按封存地质体及地理特点划分，主要包括陆上咸水层封存、海底咸水层封存、陆上枯竭油气田封存和海底枯竭油气田封存等方式。长期安全性和可靠性是CO2地质封存技术发展所面临的主要障碍。全球陆上理论封存容量为
+              6~42 万亿吨，海底理论封存容量为 2~13
+              万亿吨。我国理论地质封存潜力约为1.21~
+              4.13万亿吨，在世界主要国家及地区中仅次于北美，远超亚洲其他地区总和以及欧洲。{" "}
+            </p>
+          </div>
+
+          <img
+            style={{ width: "45%" }}
+            src={
+              AliOss + `/new_version_0518/information/details_6/table_11.png`
+            }
+            alt=""
+          />
+        </div>
+
+        <div
+          className="col_t"
+          style={{ color: ThemeColor, fontWeight: "bold", marginTop: "0.5rem" }}
+        >
+          CCUS重要意义
+        </div>
+        <p className="col">
+          一、CCUS是降低已运行的火电项目（以煤电为代表）碳排放的最重要方式。这些电站碳排放量大，仅煤电就贡献了全球三分之一的CO2。为了保障电网稳定和供电需求，通过CCUS手段降低排放，对实现碳中和目标至关重要。
+        </p>
+        <p className="col">
+          二、CCUS能够迅速提供大规模制造低成本“蓝氢”方式。“蓝氢”是指装备CCUS的化石燃料制氢手段，比“灰氢”（未装备CCUS的化石燃料制氢方式）更有效控制碳排放，同时成本仅为“绿氢”（可再生能源电解水制氢）一半。在至少10年内，是最具优势的制氢方式。
+        </p>
+        <p className="col">
+          三、CCUS是水泥钢铁等项目的关键脱碳方式。水泥和钢铁生产排放的CO2占全球总排放约15%。在其生产过程中，因化学反应产生大量CO2，无法单纯通过清洁能源替代避免碳排放。CCUS是唯一或最高效实现深度减排的手段。
+        </p>
+
+        <div
+          style={{
+            display: "flex",
+            margin: "0.2rem 0",
+            justifyContent: "space-between",
+          }}
+        >
+          <img
+            style={{ width: "45%" }}
+            src={
+              AliOss + `/new_version_0518/information/details_6/table_12.png`
+            }
+            alt=""
+          />
+          <img
+            style={{ width: "50%" }}
+            src={
+              AliOss + `/new_version_0518/information/details_6/table_13.png`
+            }
+            alt=""
+          />
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            marginTop: "0.5rem",
+            height: "2.3rem",
+          }}
+        >
+          <div
+            style={{
+              flex: 4,
+              display: "flex",
+              flexDirection: "column",
+              height: "100%",
+              //   alignItems: "center",
+              paddingRight: "0.3rem",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              className="col_t"
+              style={{
+                color: ThemeColor,
+                fontWeight: "bold",
+              }}
+            >
+              CCUS市场空间
+            </div>
+            <p className="col">
+              作为世界最大的煤炭生产国和消费国，为实现碳中和目标，中国将逐步淘汰未使用CCUS技术的燃煤电厂在整个经济系统中的运行。根据《2020中国碳中和综合报告》2040-2045年，应淘汰未采取CCUS技术的燃煤发电；2055-2060年，淘汰未采取CCUS技术的燃煤在难以实现脱碳工业部门的应用。根据《中国CCUS技术发展趋势分析》等政策性顶层设计进行预测，我国由CCUS技术处理带来的产值增加量在2050年将达到5700亿元，年复合增长率为11.3%。
+            </p>
+          </div>
+
+          <img
+            style={{ width: "45%" }}
+            src={
+              AliOss + `/new_version_0518/information/details_6/table_14.png`
+            }
+            alt=""
+          />
+        </div>
+
+        <div
+          className="col_t"
+          style={{ color: ThemeColor, fontWeight: "bold", marginTop: "0.5rem" }}
+        >
+          CCUS国内现状
+        </div>
+        <p className="col">
+          国外CCUS项目起步早，规模较大。相比国外，我国CCUS
+          项目起步较晚，且规模较小。我国已投运或建设中的CCUS
+          示范项目多以石油、煤化工、电力行业小规模的捕集驱油示范为主，缺乏大规模的多种技术组合的全流程工业化示范。截至2022年4月，全球商业化运行的CCS项目（与CCUS相比少了碳利用环节）有135个，其中北美78个，欧洲38个，中国还只有6个。从技术端上来看，中国目前大部分项目仍处在基础研究、中试阶段或工业示范阶段。
+        </p>
+
+        <div
+          className="col_t"
+          style={{ color: ThemeColor, fontWeight: "bold", marginTop: "0.5rem" }}
+        >
+          CCUS发展限制
+        </div>
+        <p className="col">
+          在上文中我们尽管提到了发展CCUS是符合我国国情的必须之途，CCUS潜在的巨大市场容量以及我国发展CCUS的得天独厚的地理封存优势。然而发展CCUS并不是一马平川的坦途，高企的碳捕集成本以及下游应用端低盈利的特点限制了现阶段整个行业的发展，一、
+          碳捕集环节成本高企目前使用的第一代碳捕集技术(燃烧后捕集技术、燃烧前捕集技术、富氧燃烧技术)发展渐趋成熟，但成本和能耗偏高、每吨成本在300-450元之间，且缺乏广泛的大规模示范工程经验；而第二代技术(如新型膜分离技术、新型吸收技术等)仍处于实验室研发或小试阶段，技术成熟后，其能耗和成本会比成熟的第一代技术降低30%以上，而技术成熟的时间点可能在2035年前后才有望大规模推广应用。二、
+          碳利用环节盈利困难虽然利用CO2驱油已经比较成熟且能实现一定的收益，但我国相关的地质利用技术较薄弱。另一方面，大规模的CO2埋存一般依靠地质地层的压力维持其液体状态，维持埋存环境的密闭性至关重要，目前CO2封存的防泄漏技术尚不成熟，CO2埋存尚存在安全隐患。作为支撑CCUS盈利端的另外一个支柱的化工利用，产值规模只有地质利用的六分之一左右。另一方面，被期待能大幅拉动碳利用产值规模、提高行业收益能力的二氧化碳制塑料技术却同样因为成本压力以及投资风险迟迟无法商用化，使得CCUS的盈利之路愈显艰难{" "}
+        </p>
+
+        <div
+          className="col_t"
+          style={{ color: ThemeColor, fontWeight: "bold", marginTop: "0.5rem" }}
+        >
+          CCUS未来之路
+        </div>
+        <p className="col">
+          随着我国碳排放交易市场的开启，国内碳配额价格虽然有所上升，但是相比国外，价格仍然处于低位，上升空间巨大。未来随着碳捕集第二代技术的普及，氢能源需求的放量，叠加我国得天独厚的地理优势以及在下游特定化工领域（CO2制塑料等）的领先地位，CCUS技术的优势将越发突显。{" "}
+        </p>
+
+        <div
+          style={{
+            display: "flex",
+            margin: "0.2rem 0",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={{ width: "45%" }}>
+            <img
+              style={{ width: "100%" }}
+              src={
+                AliOss + `/new_version_0518/information/details_6/table_15.png`
+              }
+              alt=""
+            />
+            <p className="col" style={{
+                textAlign:"center",
+                color:"rgba(0,0,0,0.7"
+            }}>燃煤电厂燃烧成本推移图</p>
+          </div>
+          <div style={{ width: "50%" }}>
+            <img
+              style={{ width: "100%" }}
+              src={
+                AliOss + `/new_version_0518/information/details_6/table_16.png`
+              }
+              alt=""
+            />
+             <p className="col" style={{
+                textAlign:"center", color:"rgba(0,0,0,0.7"
+            }}> 欧洲、上海碳配额价格推移图</p>
+          </div>
+        </div>
+        <p className="col3" style={{ paddingBottom: "0",paddingTop:"0.2rem" }}>
+        信息来源：3060
+        </p>
+        <p className="col3" style={{ paddingBottom: "0.1rem" }}>
+        内容来源：上元汇
+        </p>
       </div>
     </div>
   );
