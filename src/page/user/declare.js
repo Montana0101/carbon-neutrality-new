817ae389af02508f5d51e503b63f7f2675c843c6
montana <montana0101@outlook.com>
2022-08-23 09:30:35 +0800

    上传文件

--- src/page/user/declare.js
+++ src/page/user/declare.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { withRouter, useHistory } from "react-router-dom";
 import { getDeclareDetail } from "../../apis/index";
 import { ThemeColor, CutLine } from "../../lib/const";
-import { DatePicker, Upload, Button } from "antd";
+import { DatePicker, Upload, Button,message} from "antd";
 import "./declare.less";
 import { ButtonCmt } from "../../component/button";
 import "moment/locale/zh-cn";
@@ -34,39 +34,34 @@ const titles = [
 
 // 上传报表
 const UpdateCmt = () => {
-  const [fileList, setFileList] = useState([
-    // {
-    //   uid: "-1",
-    //   name: "xxx.png",
-    //   status: "done",
-    //   url: "http://www.baidu.com/xxx.png",
-    // },
-  ]);
 
-  const handleChange = (info) => {
-    let newFileList = [...info.fileList]; // 1. Limit the number of uploaded files
-    // Only to show two recent uploaded files, and old ones will be replaced by the new
-    newFileList = fileList.slice(-2); // 2. Read from response and show file link
-
-    newFileList = fileList.map((file) => {
-      if (file.response) {
-        // Component will show file.url as link
-        file.url = file.response.url;
-        console.log("回传的", file);
-      }
-
-      return file;
-    });
-    setFileList(newFileList);
-  };
 
   const props = {
-    action: "https://api.stiacn-app.com/stiacn-app/oss/fileUpload",
-    onChange: handleChange,
-    multiple: true,
+    name: 'file',
+    action: 'https://api.stiacn.com/stiacn-app/oss/fileUpload',
+    headers: {
+      // authorization: 'authorization-text',
+    },
+    data:{
+      description:"",
+    },
+    method:"POST",
+    onChange(info) {
+      console.log("打印下数据",info)
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+  
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} 上传成功`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} 上传失败`);
+      }
+    },
   };
+  
   return (
-    <Upload {...props} fileList={fileList}>
+    <Upload {...props}>
       <Button icon={<UploadOutlined />}></Button>
     </Upload>
   );
